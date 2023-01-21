ccf487850b36630b2dc1ec207ce6e2b4c08e8d49
Jimmy Murillo <salchichongallo1997@gmail.com>
2023-01-21 10:53:33 -0500

    feat(timer): use timer app

---
+++ timer/lib/timer_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:timer/timer/view/timer_page.dart';
+
+class TimerApp extends StatelessWidget {
+  const TimerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Timer',
+      home: TimerPage(),
+    );
+  }
+}
