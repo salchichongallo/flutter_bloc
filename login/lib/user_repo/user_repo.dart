8a451ebcaa029398c1e9c273a67312937a49e1ee
Jimmy Murillo <salchichongallo1997@gmail.com>
2023-01-21 11:21:06 -0500

    feat(login): add user repository

---
+++ login/lib/user_repo/user_repo.dart
@@ -0,0 +1,14 @@
+import 'package:login/user_repo/models/user.dart';
+import 'package:uuid/uuid.dart';
+
+class UserRepository {
+  User? _user;
+
+  Future<User?> getUser() async {
+    if (_user != null) return _user;
+    return Future.delayed(
+      const Duration(milliseconds: 300),
+      () => _user = User(const Uuid().v4()),
+    );
+  }
+}
